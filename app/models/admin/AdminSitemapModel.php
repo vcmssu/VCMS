c44c0c2c236f4f016878f0744b84bf40902e9bbc
createsite <createsite@mail.ru>
2017-01-12 12:02:43 +0700

    Исправлена генерация карты сайта. Теперь в генерации участвует БД, а не парсится сам сайт, что исключает добавление в карту сайта ненужных ссылок. Собственно скорость работы увеличена. Протестировано на создании 6 сайтмапов по 50 тысяч ссылок.

--- app/models/admin/AdminSitemapModel.php
+++ app/models/admin/AdminSitemapModel.php
@@ -22,131 +22,97 @@ class AdminSitemapModel {
 
         // Поможет при длительном выполнении скрипта
         set_time_limit(0);
-        // create sitemap
+
         $sitemap = new Sitemap('sitemap/sitemap.xml');
 
-        $scheme = strtolower(substr($_SERVER["SERVER_PROTOCOL"], 0, 5)) == 'https' ? 'https' : 'http';
-
-        $scheme = isset($_SERVER["HTTPS"]) ? 'https://' : 'http://';
-
-        $host = str_replace($scheme, '', Cms::setup('home')); // Хост сайта
-
-        $urls = array(); // Здесь будут храниться собранные ссылки
-        $content = NULL; // Рабочая переменная
-        // Здесь ссылки, которые не должны попасть в sitemap.xml
-        $nofollow = array(
-            '/search/',
-            '/404/',
-            '/go/'
-        );
-        // Первой ссылкой будет главная страница сайта, ставим ей 0, т.к. она ещё не проверена
-        $urls[$scheme . $host] = '0';
-        // Разрешённые расширения файлов, чтобы не вносить в карту сайта ссылки на медиа файлы. Также разрешены страницы без разрешения, у меня таких страниц подавляющее большинство.
-        $extensions[] = 'php';
-        $extensions[] = 'aspx';
-        $extensions[] = 'htm';
-        $extensions[] = 'html';
-        $extensions[] = 'asp';
-        $extensions[] = 'cgi';
-        $extensions[] = 'pl';
-        // Корневая директория сайта, значение можно взять из $_SERVER['DOCUMENT_ROOT'].'/';
-        $engine_root = 'sitemap/';
-
-        // Функция для сбора ссылок
-        function sitemap_geturls($page, $host, $scheme, $nofollow, $extensions, $urls) {
-            //Возможно уже проверяли эту страницу
-            if ($urls[$page] == 1) {
-                continue;
-            }
-            //Получаем содержимое ссылки. если недоступна, то заканчиваем работу функции и удаляем эту страницу из списка
-            $content = file_get_contents($page);
-            if (!$content) {
-                unset($urls[$page]);
-                return false;
-            }
-            //Отмечаем ссылку как проверенную (мы на ней побывали)
-            $urls[$page] = 0;
-            //Проверяем не стоит ли запрещающий индексировать ссылки на этой странице мета-тег с nofollow|noindex|none
-            if (preg_match('/<[Mm][Ee][Tt][Aa].*[Nn][Aa][Mm][Ee]=.?("|\'|).*[Rr][Oo][Bb][Oo][Tt][Ss].*?("|\'|).*?[Cc][Oo][Nn][Tt][Ee][Nn][Tt]=.*?("|\'|).*([Nn][Oo][Ff][Oo][Ll][Ll][Oo][Ww]|[Nn][Oo][Ii][Nn][Dd][Ee][Xx]|[Nn][Oo][Nn][Ee]).*?("|\'|).*>/', $content)) {
-                $content = NULL;
-            }
-            //Собираем все ссылки со страницы во временный массив, с помощью регулярного выражения.
-            preg_match_all("/<[Aa][\s]{1}[^>]*[Hh][Rr][Ee][Ff][^=]*=[ '\"\s]*([^ \"'>\s#]+)[^>]*>/", $content, $tmp);
-            $content = NULL;
-            //Добавляем в массив links все ссылки не имеющие аттрибут nofollow
-            foreach ($tmp[0] as $k => $v) {
-                if (!preg_match('/<.*[Rr][Ee][Ll]=.?("|\'|).*[Nn][Oo][Ff][Oo][Ll][Ll][Oo][Ww].*?("|\'|).*/', $v)) {
-                    $links[$k] = $tmp[1][$k];
-                }
-            }
-            unset($tmp);
-            //Обрабатываем полученные ссылки, отбрасываем "плохие", а потом и с них собираем...
-            for ($i = 0; $i < count($links); $i++) {
-                //Если не установлена схема и хост ссылки, то подставляем наш хост
-                if (!strstr($links[$i], $scheme . $host)) {
-                    if (isset($urlinfo['host']) AND $urlinfo['host'] . '/' != $host)
-                        $links[$i] = $links[$i];
-                    else
-                        $links[$i] = $scheme . $host . $links[$i];
-                }
-                //Убираем якори у ссылок
-                $links[$i] = preg_replace("/#.*/X", "", $links[$i]);
-                //Узнаём информацию о ссылке
-                $urlinfo = @parse_url($links[$i]);
-                if (!isset($urlinfo['path'])) {
-                    $urlinfo['path'] = NULL;
-                }
-                //Если хост совсем не наш, ссылка на главную, на почту или мы её уже обрабатывали - то заканчиваем работу с этой ссылкой
-                if ((isset($urlinfo['host']) AND $urlinfo['host'] != $host) OR $urlinfo['path'] == '/' OR isset($urls[$links[$i]]) OR strstr($links[$i], '@')) {
-                    continue;
-                }
-                //Если ссылка в нашем запрещающем списке, то также прекращаем с ней работать
-                $nofoll = 0;
-                if ($nofollow != NULL) {
-                    foreach ($nofollow as $of) {
-                        if (strstr($links[$i], $of)) {
-                            $nofoll = 1;
-                            break;
-                        }
-                    }
-                }
-                if ($nofoll == 1) {
-                    continue;
-                }
-                //Если задано расширение ссылки и оно не разрешёно, то ссылка не проходит
-                $ext = end(explode('.', $urlinfo['path']));
-                $noext = 0;
-                if ($ext != '' AND strstr($urlinfo['path'], '.') AND count($extensions) != 0) {
-                    $noext = 1;
-                    foreach ($extensions as $of) {
-                        if ($ext == $of) {
-                            $noext = 0;
-                            continue;
-                        }
-                    }
-                }
-                if ($noext == 1) {
-                    continue;
-                }
-                //Заносим ссылку в массив и отмечаем непроверенной (с неё мы ещё не забирали другие ссылки)
-                $urls[$links[$i]] = 0;
-                //Проверяем ссылки с этой страницы
-                sitemap_geturls($links[$i], $host, $scheme, $nofollow, $extensions, $urls);
-            }
-            return true;
+        //новости
+        $req = DB::run("SELECT * FROM `news` WHERE `status`='1' ORDER BY `id` DESC");
+        while ($row = $req->fetch(PDO::FETCH_ASSOC)) {
+            $url = Cms::setup('home') . '/news/' . $row['id'] . '-' . $row['translate'];
+            $sitemapTXT .= "\r\n" . $url;
+            $sitemap->addItem($url, time(), Cms::setup('sitemap_changefreq'), Cms::setup('sitemap_priority'));
+        }
+        
+        //категории ЗЦ
+        $req = DB::run("SELECT * FROM `category` ORDER BY `realid` ASC");
+        while ($row = $req->fetch(PDO::FETCH_ASSOC)) {
+            $url = Cms::setup('home') . '/download/' . $row['id'];
+            $sitemapTXT .= "\r\n" . $url;
+            $sitemap->addItem($url, time(), Cms::setup('sitemap_changefreq'), Cms::setup('sitemap_priority'));
+        }
+        
+        //файлы
+        $req = DB::run("SELECT * FROM `files` ORDER BY `id` DESC");
+        while ($row = $req->fetch(PDO::FETCH_ASSOC)) {
+            $url = Cms::setup('home') . '/download/' . $row['id'] . '-' . $row['translate'];
+            $sitemapTXT .= "\r\n" . $url;
+            $sitemap->addItem($url, time(), Cms::setup('sitemap_changefreq'), Cms::setup('sitemap_priority'));
+        }
+        
+        //категории форума
+        $req = DB::run("SELECT * FROM `forum` WHERE `refid`='0' AND `type`='0' ORDER BY `realid` ASC");
+        while ($row = $req->fetch(PDO::FETCH_ASSOC)) {
+            $url = Cms::setup('home') . '/forum/' . $row['id'];
+            $sitemapTXT .= "\r\n" . $url;
+            $sitemap->addItem($url, time(), Cms::setup('sitemap_changefreq'), Cms::setup('sitemap_priority'));
+        }
+        
+        //подкатегории форума
+        $req = DB::run("SELECT * FROM `forum` WHERE `refid`>'0' AND `type`='0' ORDER BY `realid` ASC");
+        while ($row = $req->fetch(PDO::FETCH_ASSOC)) {
+            $url = Cms::setup('home') . '/forum/' . $row['refid'].'/'.$row['id'];
+            $sitemapTXT .= "\r\n" . $url;
+            $sitemap->addItem($url, time(), Cms::setup('sitemap_changefreq'), Cms::setup('sitemap_priority'));
+        }
+        
+        //темы форума
+        $req = DB::run("SELECT * FROM `tema` WHERE `type`='0' ORDER BY `id` ASC");
+        while ($row = $req->fetch(PDO::FETCH_ASSOC)) {
+            $url = Cms::setup('home') . '/forum/' . $row['id_razdel'].'/'.$row['id_forum'].'/'.$row['id'];
+            $sitemapTXT .= "\r\n" . $url;
+            $sitemap->addItem($url, time(), Cms::setup('sitemap_changefreq'), Cms::setup('sitemap_priority'));
+        }
+        
+        //пользователи
+        $req = DB::run("SELECT * FROM `users` ORDER BY `id` ASC");
+        while ($row = $req->fetch(PDO::FETCH_ASSOC)) {
+            $url = Cms::setup('home') . '/id' . $row['id'];
+            $sitemapTXT .= "\r\n" . $url;
+            $sitemap->addItem($url, time(), Cms::setup('sitemap_changefreq'), Cms::setup('sitemap_priority'));
+        }
+        
+        //категории блогов
+        $req = DB::run("SELECT * FROM `blog_category` ORDER BY `realid` ASC");
+        while ($row = $req->fetch(PDO::FETCH_ASSOC)) {
+            $url = Cms::setup('home') . '/blogs/' . $row['id'];
+            $sitemapTXT .= "\r\n" . $url;
+            $sitemap->addItem($url, time(), Cms::setup('sitemap_changefreq'), Cms::setup('sitemap_priority'));
+        }
+        
+        //псоты блога
+        $req = DB::run("SELECT * FROM `blog` ORDER BY `id` ASC");
+        while ($row = $req->fetch(PDO::FETCH_ASSOC)) {
+            $url = Cms::setup('home') . '/blogs/' . $row['refid'].'/'.$row['id'].'-'.$row['translate'];
+            $sitemapTXT .= "\r\n" . $url;
+            $sitemap->addItem($url, time(), Cms::setup('sitemap_changefreq'), Cms::setup('sitemap_priority'));
+        }
+        
+        //галереи
+        $req = DB::run("SELECT * FROM `gallery` ORDER BY `id` ASC");
+        while ($row = $req->fetch(PDO::FETCH_ASSOC)) {
+            $url = Cms::setup('home') . '/gallery/' . $row['id'];
+            $sitemapTXT .= "\r\n" . $url;
+            $sitemap->addItem($url, time(), Cms::setup('sitemap_changefreq'), Cms::setup('sitemap_priority'));
+        }
+        
+        //фотографии
+        $req = DB::run("SELECT * FROM `gallery_photo` ORDER BY `id` ASC");
+        while ($row = $req->fetch(PDO::FETCH_ASSOC)) {
+            $url = Cms::setup('home') . '/gallery/' . $row['id_gallery'].'/'.$row['id'];
+            $sitemapTXT .= "\r\n" . $url;
+            $sitemap->addItem($url, time(), Cms::setup('sitemap_changefreq'), Cms::setup('sitemap_priority'));
         }
 
-        // (START!) Первоначальный старт функции для проверки главной страницы и последующих
-        sitemap_geturls($scheme . $host, $host, $scheme, $nofollow, $extensions, $urls);
-
-        $sitemapTXT = NULL;
-
-        // Добавляем каждую ссылку
-        foreach ($urls as $k => $v) {
-            $sitemapXML .= "\r\n<url><loc>{$k}</loc><changefreq>" . Cms::setup('sitemap_changefreq') . "</changefreq><priority>" . Cms::setup('sitemap_priority') . "</priority></url>";
-            $sitemapTXT .= "\r\n" . $k;
-            $sitemap->addItem($k, time(), Cms::setup('sitemap_changefreq'), Cms::setup('sitemap_priority'));
-        }
 
         $sitemap->write();
         if (Cms::setup('sitemap_index') == 1) {
@@ -157,33 +123,19 @@ class AdminSitemapModel {
             }
             $index->write();
         }
-
-        $sitemapTXT = trim(strtr($sitemapTXT, array(
-            '%2F' => '/',
-            '%3A' => ':',
-            '%3F' => '?',
-            '%3D' => '=',
-            '%26' => '&',
-            '%27' => "'",
-            '%22' => '"',
-            '%3E' => '>',
-            '%3C' => '<',
-            '%23' => '#',
-            '&' => '&'
-        )));
-
-
+        
         //Запись в файл
         if (Cms::setup('sitemap_txt') == 1) {
-            $fp = fopen($engine_root . 'sitemap.txt', 'w+');
+            $fp = fopen('sitemap/sitemap.txt', 'w+');
             if (!fwrite($fp, $sitemapTXT)) {
                 echo 'Ошибка записи!';
             }
             fclose($fp);
         }
 
-        if (Cms::setup('adminlogs') == 1)
-            Cms::adminlogs('Карта сайта', 'Сгенерирована карта сайта'); //пишем лог админа, если включено
+        if (Cms::setup('adminlogs') == 1) {
+            Cms::adminlogs('Карта сайта', 'Сгенерирована карта сайта');
+        } //пишем лог админа, если включено
 
         Functions::redirect(Cms::setup('adminpanel') . '/sitemap');
     }
